ee97e025f6539ca0ef3c83f8fd8b02e489d94545
maciektr <mtratnowiecki@gmail.com>
2021-01-17 23:36:13 +0100

    foata from graph

--- lab10/dgraph.py
+++ lab10/dgraph.py
@@ -6,6 +6,7 @@ from read_input import Problem
 class DependencyGraph(nx.DiGraph):
     def __init__(self, problem: Problem, dependency_relation, *args, **kwargs):
         super().__init__(directed=True, *args, **kwargs)
+        self.problem = problem
         for i in range(len(problem.word)):
             self.add_node(i, char=problem.word[i])
             for j in range(i):
@@ -30,14 +31,16 @@ class DependencyGraph(nx.DiGraph):
             delete = filter(lambda e: self.contains_edge(e), map(lambda n: (node, n), delete))
             self.remove_edges_from(delete)
 
-    def dfs(self, node, visited):
+    def dfs(self, node, visited, topological=None):
         if node in visited:
             return
         visited.add(node)
         for out_edge in self.out_edges(node):
             neighbour = out_edge[1]
             if neighbour not in visited:
-                self.dfs(neighbour, visited)
+                self.dfs(neighbour, visited, topological=topological)
+        if topological is not None:
+            topological.append(node)
 
     def contains_edge(self, edge):
         return edge in self.edges
@@ -54,8 +57,39 @@ class DependencyGraph(nx.DiGraph):
             map(lambda x: (x[0],x[1]), nx.get_node_attributes(self, 'char').items())))
         nx.draw_networkx_edges(self, pos, edgelist=self.edges(), edge_cmap=plt.cm.Wistia,
                                width=2, arrows=True, arrowstyle='-|>', arrowsize=20, node_size=1000)
-        # nx.draw_networkx_edge_labels(self, pos, dict(
-            # map(lambda x: (x[0], str(round(x[1], 2)) + 'A'), nx.get_edge_attributes(self, 'current').items())))
         plt.draw()
         plt.show()
 
+    def get_foata(self):
+        result = []
+        topo = self.topological_sort()
+        i = 0
+        while i < len(self.problem.word):
+            block = '('
+            block += topo[i]
+
+            k = i
+            i += 1
+            while i < len(self.problem.word):
+                is_independent = True
+                for j in range(k,i):
+                    if (topo[i] not in self.problem.relation) \
+                        or (topo[j] not in self.problem.relation[topo[i]]):
+                        is_independent = False
+                        break
+                if is_independent:
+                    block += topo[i]
+                    i+=1
+                else:
+                    break
+            block += ')'
+            result.append(block)
+        return result
+
+    def topological_sort(self):
+        visited = set()
+        topo = []
+        for node in self.nodes:
+            self.dfs(node, visited, topological=topo)
+        topo = list(map(lambda x: self.node_char(x), reversed(topo)))
+        return topo
