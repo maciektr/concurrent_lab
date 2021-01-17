d0c9aa546b8114685e1cf42319b30c39c44715df
maciektr <mtratnowiecki@gmail.com>
2021-01-17 23:35:05 +0100

    Refactor

--- lab10/main.py
+++ lab10/main.py
@@ -5,18 +5,18 @@ from dgraph import DependencyGraph
 
 def main():
     problem = read_input(vocal=False)
-    # print(problem)
+    # print('Problem', problem)
     solver = Solver(problem)
 
-    # dep = solver.dependency_relation
-    # print(dep)
-    # traces = solver.get_trace()
-    # print(traces)
-    # floata = solver.get_floata()
-    # print(floata)
-
-    solver.get_dependency_graph().plot()
-
+    dep = solver.dependency_relation
+    print('Dependency graph', dep)
+    traces = solver.get_trace()
+    print('Traces', traces)
+    foata = solver.get_foata()
+    print('Foata', foata)
+    foata = solver.get_foata_from_graph()
+    print('Foata', foata)
+    solver.dependency_graph.plot()
 
 if __name__ == '__main__':
     main()
--- lab10/solve.py
+++ lab10/solve.py
@@ -6,75 +6,77 @@ class Solver:
     def __init__(self, problem: Problem):
         self.problem = problem
         self.__dependency_relation = None
+        self.__dgraph = None
+
+    @staticmethod
+    def __not_in(key, value, rel):
+        return (key not in rel) or (value not in rel[key])
+
+    @staticmethod
+    def __safe_add(key, val, rel):
+        if key not in rel:
+            rel[key] = set([val])
+        else:
+            rel[key].add(val)
 
     @property
     def dependency_relation(self):
         if self.__dependency_relation:
             return self.__dependency_relation
 
+        def all_pairs(arr):
+            for a in arr:
+                for b in arr:
+                    yield (a,b)
+
         dependency = {}
-        for a in self.problem.alphabet:
-            for b in self.problem.alphabet:
-                if (a not in self.problem.relation) \
-                    or (b not in self.problem.relation[a]):
-                    if a not in dependency:
-                        dependency[a] = set([b])
-                    else:
-                        dependency[a].add(b)
+        for (a,b) in all_pairs(self.problem.alphabet):
+            if self.__not_in(a,b,self.problem.relation):
+                self.__safe_add(a,b,dependency)
 
         self.__dependency_relation = dependency
         return dependency
 
     def get_trace(self, word=None, traces=None):
-        # print(word, traces, problem, sep='\n')
-        if not word:
-            word = self.problem.word
-        if not traces:
-            traces = set([word])
+        word = word if word else self.problem.word
+        traces = traces if traces else set()
         traces.add(word)
         i = 0
         while i < len(word) - 1:
             char = word[i]
             changed = word[:i] + word[i+1] + word[i] + word[i+2:]
-            if char in self.problem.relation \
-                and word[i+1] in self.problem.relation[char] \
+            if not self.__not_in(char, word[i+1], self.problem.relation) \
                 and changed not in traces:
                 traces |= self.get_trace(word=changed, traces=traces)
                 i+=1
             i+=1
         return traces
 
-    def get_floata(self):
+    def get_foata(self):
         result = []
-        dependency = self.dependency_relation
-
         stacks = {char: [] for char in self.problem.alphabet}
+
         for i in range(len(self.problem.word)-1, -1, -1):
             char = self.problem.word[i]
             stacks[char].append(char)
             for char2 in self.problem.alphabet:
                 if char != char2 \
-                    and (char not in self.problem.relation
-                        or char2 not in self.problem.relation[char]
-                    ):
+                    and self.__not_in(char, char2, self.problem.relation):
                     stacks[char2].append('#')
 
         while True:
             block = '('
             not_empty_stack = False
             popped_markers = {char: 0 for char in self.problem.alphabet}
-
             for key in stacks.keys():
                 if len(stacks[key]) == 0:
                     continue
                 not_empty_stack = True
-
                 if stacks[key][-1] == '#' or popped_markers[key] != 0:
                     continue
-
                 popped = stacks[key].pop()
                 block+=popped
-                for char in dependency[popped]:
+                for char in self.dependency_relation[popped]:
                     if char != popped \
                         and len(stacks[char]) != 0:
                         stacks[char].pop()
@@ -87,6 +89,11 @@ class Solver:
 
         return result
 
-    def get_dependency_graph(self):
-        dgraph = DependencyGraph(self.problem, self.dependency_relation)
-        return dgraph
+    @property
+    def dependency_graph(self):
+        if not self.__dgraph:
+            self.__dgraph = DependencyGraph(self.problem, self.dependency_relation)
+        return self.__dgraph
+
+    def get_foata_from_graph(self):
+        return self.dependency_graph.get_foata()
